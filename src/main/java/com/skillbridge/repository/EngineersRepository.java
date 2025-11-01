be4beb9ba4c7a95e90688bc9521c0b4153f41d76
Quang-vip <lap1234.lpo@gmail.com>
2025-11-01 18:58:09 +0700

    Initialize Homepage's code
    Initializing Homepage's base backend code

---
+++ src/main/java/com/skillbridge/repository/EngineersRepository.java
@@ -0,0 +1,8 @@
+package com.skillbridge.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EngineersRepository extends JpaRepository<> {
+}
